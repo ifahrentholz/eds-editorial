ce7cab5093b86400e6a303bd9ee35f824c021238
Samuele Calamita <samuele.calamita@diva-e.com>
2024-02-02 11:53:52 +0100

    added banner rendering

--- src/blocks/banner/banner.ts
+++ src/blocks/banner/banner.ts
@@ -1,53 +1,48 @@
-import { html, render } from "lit";
-import { unsafeHTML } from "lit-html/directives/unsafe-html";
+import { html, render } from 'lit';
+import { unsafeHTML } from 'lit-html/directives/unsafe-html.js';
 
 interface TemplateArgs {
-  headline: string;
-  subline: string;
-  text: string;
-  buttons: HTMLAnchorElement[];
+  headline?: string;
+  subline?: string;
+  picture?: HTMLPictureElement;
+  texts?: HTMLParagraphElement[];
+  buttons?: HTMLAnchorElement[];
 }
 
-const template = ({ headline, subline, text, buttons }: TemplateArgs) => html`
-  <section id="banner">
-    <div class="content">
-      <header>
-        <h1>${headline}</h1>
-        <p>${subline}</p>
-      </header>
-      ${unsafeHTML(text)}
-      <ul class="actions">
-        ${buttons.map(
-          (button) =>
-            html`<li>
-              <a href="${button}" class="button big">Learn More</a>
-            </li>`,
-        )}
-      </ul>
-    </div>
-    <span class="image object">
-      <img src="images/pic10.jpg" alt="" />
-    </span>
-  </section>
-`;
+const template = ({ headline, subline, texts, buttons, picture }: TemplateArgs) => {
+  return html`
+    <section id="banner">
+      <div class="content">
+        <header>
+          <h1>${headline}</h1>
+          <p>${subline}</p>
+        </header>
+        ${unsafeHTML(texts?.map((text) => text.innerHTML).join(''))}
+        <ul class="actions">
+          ${buttons?.map(
+            (button) =>
+              html`<li>
+                <a href="${button.href}" class="button big">${button.innerText}</a>
+              </li>`
+          )}
+        </ul>
+      </div>
+      <span class="image object"> ${picture ?? unsafeHTML(picture)} </span>
+    </section>
+  `;
+};
 
 export default function (block: HTMLElement) {
-  const firstRow = block.querySelector("div");
+  const firstRow = block.querySelector('div');
   const secondRow = block.children[1];
-  const headline = firstRow?.querySelector("h1");
-  const subline = firstRow?.querySelector("h3");
-  const text = firstRow?.querySelectorAll("p");
-  const buttons = secondRow?.querySelectorAll("a");
+  const headline = firstRow?.querySelector('h1')?.innerText;
+  const subline = firstRow?.querySelector('h3')?.innerText;
+  const texts = firstRow ? [...firstRow.querySelectorAll('p')] : [];
+  const buttons = [...secondRow?.querySelectorAll('a')];
+  const picture = firstRow?.querySelector('picture') || undefined;
 
-  console.log({
-    firstRow,
-    secondRow,
-    headline,
-    subline,
-    text,
-    buttons,
-  });
+  block.innerHTML = '';
 
-  block.style.removeProperty("display");
-  render(template, block);
+  block.style.removeProperty('display');
+  render(template({ headline, subline, texts, buttons, picture }), block);
 }
