c408b4f91670666aa548fc8103de8db4f7f3435c
Elanor Krause <elanor.krause@diva-e.com>
2024-02-02 14:22:27 +0100

    added features block

---
+++ src/blocks/features/features.ts
@@ -0,0 +1,37 @@
+import { html, render } from 'lit';
+import { unsafeHTML } from 'lit-html/directives/unsafe-html.js';
+
+interface Feature {
+  icon: string;
+  textBlock: string;
+}
+
+type TemplateArgs = Feature[];
+
+const template = (features: TemplateArgs) => {
+  return html`
+    ${features.map(
+      (feature) => html`
+        <article>
+          <span class="icon fa-gem">${feature.icon}</span>
+          <div class="content">${unsafeHTML(feature.textBlock)}</div>
+        </article>
+      `
+    )}
+  `;
+};
+
+export default function (block: HTMLElement) {
+  const rows = block.querySelectorAll(':scope > div');
+  let features: Feature[] = [];
+  [...rows].forEach((row) => {
+    const icon = row.children[0].innerHTML;
+    const textBlock = row.children[1].innerHTML;
+    features.push({ icon, textBlock });
+  });
+
+  block.innerHTML = '';
+
+  block.style.removeProperty('display');
+  render(template(features), block);
+}
