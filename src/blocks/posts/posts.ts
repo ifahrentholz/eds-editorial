a59c9cedbd8aec8be908ad2beea2c7f2efcd41f5
Elanor Krause <elanor.krause@diva-e.com>
2024-02-05 10:33:26 +0100

    added images to posts

--- src/blocks/posts/posts.ts
+++ src/blocks/posts/posts.ts
@@ -1,16 +1,18 @@
 import { html, render } from 'lit';
+import { createOptimizedPicture } from '../../utils/createOptimizedPicture';
 
 interface PostArgs {
   postUrl: string;
   headline?: string;
   text?: string;
+  picture: HTMLPictureElement;
 }
 
 const postTemplate = (args: PostArgs) => {
-  const { postUrl, headline, text } = args;
+  const { postUrl, headline, text, picture } = args;
   return html`
     <article>
-      <!-- <a href="${postUrl}" class="image"><img src="images/pic01.jpg" alt="" /></a> -->
+      <a href="${postUrl}" class="image">${picture}</a>
       <h3>${headline}</h3>
       <p>${text?.slice(0, 200)}</p>
       <ul class="actions">
@@ -29,15 +31,16 @@ export default async function (block: HTMLElement) {
 
   const req = await fetch(`${window.hlx.codeBasePath}/query-index.json`);
   const response = await req.json();
+  console.log('response: ', response);
   const data = response.data
     .filter((item) => {
       return item.path.includes('/posts');
     })
-    .map((item) => item.path);
+    .map((item) => item);
 
   const postsPreview = await Promise.all(
-    data.map(async (path) => {
-      const result = await fetch(`${window.hlx.codeBasePath}${path}.plain.html`);
+    data.map(async (post) => {
+      const result = await fetch(`${window.hlx.codeBasePath}${post.path}.plain.html`);
       return result.text();
     })
   );
@@ -49,11 +52,13 @@ export default async function (block: HTMLElement) {
 
   const posts = postsPreviewHtml.map((doc, index) => {
     return {
-      postUrl: `${window.hlx.codeBasePath}${data[index]}`,
+      postUrl: `${window.hlx.codeBasePath}${data[index].path}`,
       headline: doc.querySelector('h1')?.innerText || doc.querySelector('h2')?.innerText,
       text: doc.querySelector('p')?.innerText?.trim(),
+      picture: createOptimizedPicture({ src: data[index].image, alt: data[index].imagealt }),
     };
   });
+  console.log('posts: ', posts);
 
   block.style.removeProperty('display');
   render(template(posts), block);
---
+++ src/utils/createOptimizedPicture.ts
@@ -0,0 +1,55 @@
+/**
+ * Returns a picture element with webp and fallbacks
+ * @param {string} src The image URL
+ * @param {string} [alt] The image alternative text
+ * @param {boolean} [eager] Set loading attribute to eager
+ * @param {Array} [breakpoints] Breakpoints and corresponding params (eg. width)
+ * @returns {Element} The picture element
+ */
+
+interface CreateOptimizedPictureArgs {
+  src: string;
+  alt: string;
+  eager?: boolean;
+  breakpoints?: Array<Record<string, string>>;
+}
+
+export function createOptimizedPicture(args: CreateOptimizedPictureArgs): HTMLPictureElement {
+  const {
+    src,
+    alt,
+    eager = false,
+    breakpoints = [{ media: '(min-width: 600px)', width: '2000' }, { width: '750' }],
+  } = args;
+  const url = new URL(src, window.location.href);
+  const picture = document.createElement('picture');
+  const { pathname } = url;
+  const ext = pathname.substring(pathname.lastIndexOf('.') + 1);
+
+  // webp
+  breakpoints.forEach((br) => {
+    const source = document.createElement('source');
+    if (br.media) source.setAttribute('media', br.media);
+    source.setAttribute('type', 'image/webp');
+    source.setAttribute('srcset', `${pathname}?width=${br.width}&format=webply&optimize=medium`);
+    picture.appendChild(source);
+  });
+
+  // fallback
+  breakpoints.forEach((br, i) => {
+    if (i < breakpoints.length - 1) {
+      const source = document.createElement('source');
+      if (br.media) source.setAttribute('media', br.media);
+      source.setAttribute('srcset', `${pathname}?width=${br.width}&format=${ext}&optimize=medium`);
+      picture.appendChild(source);
+    } else {
+      const img = document.createElement('img');
+      img.setAttribute('loading', eager ? 'eager' : 'lazy');
+      img.setAttribute('alt', alt);
+      picture.appendChild(img);
+      img.setAttribute('src', `${pathname}?width=${br.width}&format=${ext}&optimize=medium`);
+    }
+  });
+
+  return picture;
+}
