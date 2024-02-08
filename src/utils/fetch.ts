1d8d36ba72394e75f7d41589680ddbd556636786
christian.schmidt <christian.schmidt@diva-e.com>
2024-02-08 11:16:49 +0100

    Added FetchCache Singleton for instance runtime cache of fetch requests

--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -1,10 +1,21 @@
+import { fetchCache } from './FetchCache.ts';
+
 const fetchData = async (endpoint: string, init?: RequestInit): Promise<Response> => {
   const decoratedEndpoint = endpoint.startsWith('/') ? endpoint : `/${endpoint}`;
+  const url = `${window.hlx.codeBasePath}${decoratedEndpoint}`;
+  const cache = fetchCache.get(url);
 
-  return await fetch(`${window.hlx.codeBasePath}${decoratedEndpoint}`, init);
+  if (cache !== null) {
+    return  cache;
+  }
+
+  const response = await fetch(url, init);
+  fetchCache.set(url, response);
+
+  return response;
 };
 
-export const fetchText = async (endpoint: string, init?: RequestInit ): Promise<string> => {
+export const fetchText = async (endpoint: string, init?: RequestInit): Promise<string> => {
   return await (await fetchData(endpoint, init)).text();
 };
 
