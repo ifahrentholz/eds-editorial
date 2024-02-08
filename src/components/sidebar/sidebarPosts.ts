b11211b248a6b6abff9424730b0cf64a28cde451
christian.schmidt <christian.schmidt@diva-e.com>
2024-02-08 11:09:54 +0100

    efactored fetch * Seperated fetchData to gain Single Responsibility * Removed Param type since only two options needed max * Added fetchJson with generic return type * Added fetchText * Used fetchData in fetchJson and fetchText for DRY
    Refactored fetching sheet data
    * Created SheetService
    * Moved all sheet related logic to SheetService

--- src/components/sidebar/sidebarPosts.ts
+++ src/components/sidebar/sidebarPosts.ts
@@ -1,39 +1,36 @@
 import { html, LitElement } from 'lit';
-import { fetchData } from '../../utils/fetchData.ts';
-import { Sitemap, SiteMapEntry } from './sidebarNav.ts';
 import { customElement, state } from 'lit/decorators.js';
 import { createOptimizedPicture } from '../../utils/createOptimizedPicture.ts';
-
-interface SheetsResponse {
-  type: string;
-  data: [];
-  offset: number;
-  total: number;
-}
+import { SheetService, Sitemap, SiteMapEntry } from '../../services/sheet.service.ts';
 
 @customElement('sidebar-posts')
 export class SidebarPosts extends LitElement {
   @state()
-  private lastThreePosts: SiteMapEntry[];
+  private lastTreePosts: Sitemap;
+  private sheetService: SheetService;
+
+  constructor() {
+    super();
+    this.sheetService = new SheetService();
+  }
 
   async connectedCallback() {
     super.connectedCallback();
-    const sitemap = await this.fetchSitemap();
-    const posts = this.getPosts(sitemap);
-    this.lastThreePosts = this.getLastThreePosts(posts);
+    const posts = await this.getPosts();
+    this.lastTreePosts = this.getLastThreePosts(posts);
   }
 
   render() {
-    if (!this.lastThreePosts) return;
+    if (!this.lastTreePosts) return;
     return html`
       <header class="major">
         <h2>Newest Posts</h2>
       </header>
-      <div class="mini-posts">${this.lastThreePosts.map((siteMapEntry) => this.renderPost(siteMapEntry))}</div>
+      <div class="mini-posts">${this.lastTreePosts.map((siteMapEntry) => this.renderPost(siteMapEntry))}</div>
     `;
 
     //TODO: Add overview if more button is needed
-    /* 
+    /*
      <ul class="actions">
         <li><a href="#" class="button">More</a></li>
       </ul>
@@ -44,8 +41,8 @@ export class SidebarPosts extends LitElement {
     return this;
   }
 
-  private getLastThreePosts(sitemap: Sitemap) {
-    sitemap.sort((sitemapEntry: SiteMapEntry, nextSitemapEntry: SiteMapEntry) => {
+  private getLastThreePosts(posts: Sitemap) {
+    posts.sort((sitemapEntry: SiteMapEntry, nextSitemapEntry: SiteMapEntry) => {
       if (sitemapEntry.lastModified > nextSitemapEntry.lastModified) {
         return -1;
       } else if (sitemapEntry.lastModified < nextSitemapEntry.lastModified) {
@@ -55,23 +52,20 @@ export class SidebarPosts extends LitElement {
       return 0;
     });
 
-    return sitemap.slice(0, 3);
-  }
-
-  private async fetchSitemap() {
-    return <Sitemap>(await fetchData<SheetsResponse>({ endpoint: '/query-index.json', getJson: true })).data;
+    return posts.slice(0, 3);
   }
 
   private renderPost(siteMapEntry: SiteMapEntry) {
     return html` <article>
       <a href="${siteMapEntry.path}" class="image">
-        ${createOptimizedPicture({ src: siteMapEntry.image, alt: siteMapEntry.imagealt })}
+        ${createOptimizedPicture({ src: siteMapEntry.image, alt: siteMapEntry.imageAlt })}
       </a>
       <p>${siteMapEntry.description}</p>
     </article>`;
   }
 
-  private getPosts(sitemap: SiteMapEntry[]) {
+  private async getPosts() {
+    const sitemap = await this.sheetService.getSiteMap();
     return sitemap.filter((item) => item.path.includes('/posts'));
   }
 }
---
+++ src/utils/fetch.ts
@@ -0,0 +1,13 @@
+const fetchData = async (endpoint: string, init?: RequestInit): Promise<Response> => {
+  const decoratedEndpoint = endpoint.startsWith('/') ? endpoint : `/${endpoint}`;
+
+  return await fetch(`${window.hlx.codeBasePath}${decoratedEndpoint}`, init);
+};
+
+export const fetchText = async (endpoint: string, init?: RequestInit ): Promise<string> => {
+  return await (await fetchData(endpoint, init)).text();
+};
+
+export const fetchJson = async <T>(endpoint: string, init?: RequestInit): Promise<T> => {
+  return await (await fetchData(endpoint, init)).json();
+};
