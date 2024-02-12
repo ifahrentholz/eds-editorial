b2a99bf2d2d64345e552ec4f5a6505fee59d4938
samuelecalamita <samuelecalamita@gmail.com>
2024-02-12 18:14:37 +0100

    solved TODO in sidebarContact

--- types/components/sidebar/sidebarContact.d.ts
+++ types/components/sidebar/sidebarContact.d.ts
@@ -1,18 +1,20 @@
-import { LitElement } from 'lit';
+import { LitElement, nothing } from 'lit';
 interface SidebarContactTemplateArgs {
-    headline: HTMLHeadingElement;
-    text: HTMLParagraphElement;
+    headline?: string;
+    text?: string;
     contacts: Contact[];
 }
 interface Contact {
-    contactIcon: string;
-    contactMarkup: string;
+    contactIcon?: string;
+    contactMarkup?: string;
 }
 export declare class SidebarContact extends LitElement {
     contactTemplateArgs: SidebarContactTemplateArgs;
     protected createRenderRoot(): HTMLElement | DocumentFragment;
     connectedCallback(): void;
     fetchContactData(): Promise<void>;
+    renderHeader(headline?: string): typeof nothing | import("lit-html").TemplateResult<1>;
+    renderText(text?: string): typeof nothing | import("lit-html").TemplateResult<1>;
     render(): import("lit-html").TemplateResult<1> | undefined;
 }
 export {};
