1f194dfdb2c71291d7ad5a4cf874b73bf47899fd
christian.schmidt <christian.schmidt@diva-e.com>
2024-02-15 15:48:08 +0100

    Fix typo

--- src/services/sitemap.service.ts
+++ src/services/sitemap.service.ts
@@ -2,16 +2,17 @@ import { SheetsResponse, Sitemap } from '../shared.types.ts';
 import { fetchJson } from '../utils/fetchJson.ts';
 
 export class SitemapService {
-  private fetchPromis: Promise<SheetsResponse>;
+  private fetchPromise: Promise<SheetsResponse>;
 
   async getSiteMap(): Promise<Sitemap> {
     return <Sitemap>(await this.getQueryIndex()).data;
   }
 
   async getQueryIndex(): Promise<SheetsResponse> {
-    if (this.fetchPromis) return this.fetchPromis; // Ensures only to start one promise for this fetch request at a time
-    this.fetchPromis = fetchJson<SheetsResponse>('/query-index.json');
-    return this.fetchPromis;
+    // Ensures only to start one promise for this fetch request at a time
+    if (this.fetchPromise) return this.fetchPromise;
+    this.fetchPromise = fetchJson<SheetsResponse>('/query-index.json');
+    return this.fetchPromise;
   }
 }
 
