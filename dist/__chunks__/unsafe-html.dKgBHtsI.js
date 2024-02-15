ef1d8637ef016fcb34e0317f4cf3010e6c37e2e0
eds-editorial Continuous Integration <eds-editorial-ci@diva-e.com>
2024-02-15 08:35:00 +0000

    Continuous Integration Build Project

---
+++ dist/__chunks__/unsafe-html.dKgBHtsI.js
@@ -0,0 +1,10 @@
+import{T as r,w as o}from"./lit-element.XkUWx5ik.js";/**
+ * @license
+ * Copyright 2017 Google LLC
+ * SPDX-License-Identifier: BSD-3-Clause
+ */const u={ATTRIBUTE:1,CHILD:2,PROPERTY:3,BOOLEAN_ATTRIBUTE:4,EVENT:5,ELEMENT:6},c=e=>(...t)=>({_$litDirective$:e,values:t});class h{constructor(t){}get _$AU(){return this._$AM._$AU}_$AT(t,i,n){this._$Ct=t,this._$AM=i,this._$Ci=n}_$AS(t,i){return this.update(t,i)}update(t,i){return this.render(...i)}}/**
+ * @license
+ * Copyright 2017 Google LLC
+ * SPDX-License-Identifier: BSD-3-Clause
+ */class s extends h{constructor(t){if(super(t),this.it=r,t.type!==u.CHILD)throw Error(this.constructor.directiveName+"() can only be used in child bindings")}render(t){if(t===r||t==null)return this._t=void 0,this.it=t;if(t===o)return t;if(typeof t!="string")throw Error(this.constructor.directiveName+"() called with a non-string value");if(t===this.it)return this._t;this.it=t;const i=[t];return i.raw=i,this._t={_$litType$:this.constructor.resultType,strings:i,values:[]}}}s.directiveName="unsafeHTML",s.resultType=1;const l=c(s);export{c as a,s as e,h as i,l as o,u as t};
+//# sourceMappingURL=unsafe-html.dKgBHtsI.js.map
