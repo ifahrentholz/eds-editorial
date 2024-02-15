dcd5d49922fc7bf48986e1bd15e364b9c492cd14
Ingo Fahrentholz <ingo.fahrentholz@diva-e.com>
2024-02-15 21:49:16 +0100

    Increase global timeout to 200ms

--- src/services/main.service.ts
+++ src/services/main.service.ts
@@ -61,7 +61,7 @@ export class MainService {
       // TODO: Performace adjustment
       setTimeout(() => {
         document.body.removeAttribute('style');
-      }, 120);
+      }, 200);
 
       // await this.waitForLCP(LCP_BLOCKS);
 
