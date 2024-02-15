ec0c97f2642d117ed264783eb71b8806124ab7b1
eds-editorial Continuous Integration <eds-editorial-ci@diva-e.com>
2024-02-15 14:21:48 +0000

    Continuous Integration Build Project

--- dist/features/features.js
+++ dist/features/features.js
@@ -1,9 +1,2 @@
-import{j as s,x as c}from"../__chunks__/lit-element.XkUWx5ik.js";import{o as a}from"../__chunks__/unsafe-html.dKgBHtsI.js";import"../__chunks__/icon.PS61ZRz5.js";import"../__chunks__/property.0daVBkvz.js";const p=e=>c`
-    ${e.map(n=>c`
-        <article>
-          <span class="icon"><icon-component class="icon-wc" name="${n.icon}"></icon-component></span>
-          <div class="content"Test ${a(n.textBlock)}</div>
-        </article>
-      `)}
-  `;function u(e){const n=e.querySelectorAll(":scope > div");let t=[];[...n].forEach(o=>{const r=o.children[0].innerHTML,i=o.children[1].innerHTML;t.push({icon:r,textBlock:i})}),e.innerHTML="",e.style.removeProperty("display"),s(p(t),e)}export{u as default};
+import{j as s,x as c}from"../__chunks__/lit-element.XkUWx5ik.js";import{o as a}from"../__chunks__/unsafe-html.dKgBHtsI.js";import"../__chunks__/icon.PS61ZRz5.js";import"../__chunks__/property.0daVBkvz.js";const p=n=>c`${n.map(e=>c`<article><span class="icon"><icon-component class="icon-wc" name="${e.icon}"></icon-component></span><div class="content">${a(e.textBlock)}</div></article>`)}`;function u(n){const e=n.querySelectorAll(":scope > div");let t=[];[...e].forEach(o=>{const r=o.children[0].innerHTML,i=o.children[1].innerHTML;t.push({icon:r,textBlock:i})}),n.innerHTML="",n.style.removeProperty("display"),s(p(t),n)}export{u as default};
 //# sourceMappingURL=features.js.map
