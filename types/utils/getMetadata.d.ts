cc5df77ff5275d335636e70a8bd6c091e0ace69a
eds-editorial Continuous Integration <eds-editorial-ci@diva-e.com>
2024-02-23 10:53:51 +0000

    Continuous Integration Build Project

--- types/utils/getMetadata.d.ts
+++ types/utils/getMetadata.d.ts
@@ -1,1 +1,19 @@
+/**
+ * Retrieves the content of a specified metadata tag from the document head.
+ *
+ * @param {string} name - The name or property attribute value of the metadata tag.
+ * @param {Document} [doc=document] - The document to search for the metadata tag (default is the current document).
+ * @returns {string} - The content of the metadata tag, if found; otherwise, an empty string.
+ *
+ @example
+* // Example 1:
+* const metaContent1 = getMetadata('description');
+* console.log(metaContent1);
+* // Output: 'This is a sample description.'
+*
+* // Example 2:
+* const metaContent2 = getMetadata('og:title', anotherDocument);
+* console.log(metaContent2);
+* // Output: 'Open Graph Title'
+*/
 export declare function getMetadata(name: string, doc?: Document): string;
