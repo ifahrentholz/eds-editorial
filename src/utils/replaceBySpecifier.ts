76a10364a9c131cf4136ad61a9f12e3676272d10
christian.schmidt <christian.schmidt@diva-e.com>
2024-02-23 11:48:28 +0100

    Add jsdoc and types to replaceBySpecifier.ts

--- src/utils/replaceBySpecifier.ts
+++ src/utils/replaceBySpecifier.ts
@@ -1,11 +1,48 @@
-type ReplaceBySpecifier = {
+/**
+ * Represents the structure of an object used to specify replacements in a string.
+ *
+ * @interface ReplaceBySpecifier
+ * @property {string} input - The input string where replacements will be made.
+ * @property {string} specifier - The specifier string to search for in the input.
+ * @property {string} htmlTag - The HTML tag to wrap around the parts matched by the specifier.
+ */
+interface ReplaceBySpecifier {
   input: string;
   specifier: string;
   htmlTag: string;
-};
+}
+
+/**
+ * Replaces occurrences of a specified specifier in a string with an HTML tag.
+ *
+ * @param {ReplaceBySpecifier} param - An object containing input string, specifier, and HTML tag.
+ * @param {string} param.input - The input string where replacements will be made.
+ * @param {string} param.specifier - The specifier string to search for in the input.
+ * @param {string} param.htmlTag - The HTML tag to wrap around the parts matched by the specifier.
+ * @returns {string} - The modified string with replacements.
+ *
+ @example
+ * // Example 1:
+  * const result1 = replaceBySpecifier({
+  *   input: 'This is a test string with some test keywords.',
+  *   specifier: 'test',
+  *   htmlTag: 'strong'
+  * });
+  * console.log(result1);
+  * // Output: 'This is a <strong>test</strong> string with some <strong>test</strong> keywords.'
+  *
+  * // Example 2:
+  * const result2 = replaceBySpecifier({
+  *   input: 'Hello, world!',
+  *   specifier: ',',
+  *   htmlTag: 'span'
+  * });
+  * console.log(result2);
+  * // Output: 'Hello<span>,</span> world!'
+ */
 export function replaceBySpecifier({ input, specifier, htmlTag }: ReplaceBySpecifier): string {
   return input
     .split(specifier)
-    .map((part, index) => (index % 2 === 1 ? `<${htmlTag}>${part}</${htmlTag}>` : part))
+    .map((part: string, index: number): string => (index % 2 === 1 ? `<${htmlTag}>${part}</${htmlTag}>` : part))
     .join('');
 }
