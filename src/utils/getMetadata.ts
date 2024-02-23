935b1a12fd92ac8586e8c0465ee8016825e70366
christian.schmidt <christian.schmidt@diva-e.com>
2024-02-23 11:28:19 +0100

    Added doc and types for getMetadata.ts

--- src/utils/getMetadata.ts
+++ src/utils/getMetadata.ts
@@ -1,11 +1,14 @@
-export function getMetadata(name: string, doc = document) {
-  const attr = name && name.includes(':') ? 'property' : 'name';
+/**
+ * Retrieves the content of a specified metadata tag from the document head.
+ *
+ * @param {string} name - The name or property attribute value of the metadata tag.
+ * @param {Document} [doc=document] - The document to search for the metadata tag (default is the current document).
+ * @returns {string} - The content of the metadata tag, if found; otherwise, an empty string.
+ */
+export function getMetadata(name: string, doc: Document = document): string {
+  const attr: 'property' | 'name' = name && name.includes(':') ? 'property' : 'name';
   const metaTags: HTMLMetaElement[] =
     ([...doc.head.querySelectorAll(`meta[${attr}="${name}"]`)] as HTMLMetaElement[]) || [];
-  const meta = metaTags
-    .map((m) => {
-      return m.content;
-    })
-    .join(', ');
+  const meta: string = metaTags.map((metaElement: HTMLMetaElement) => metaElement.content).join(', ');
   return meta.length ? meta : '';
 }
