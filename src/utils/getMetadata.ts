1cbfe536e1cb98b8de9c4ff16c5312b8dfcab199
christian.schmidt <christian.schmidt@diva-e.com>
2024-02-26 10:47:24 +0100

    Review adjustments * Add meta tag to the examples * Renamed name property to value

--- src/utils/getMetadata.ts
+++ src/utils/getMetadata.ts
@@ -5,21 +5,24 @@
  * @param {Document} [doc=document] - The document to search for the metadata tag (default is the current document).
  * @returns {string} - The content of the metadata tag, if found; otherwise, an empty string.
  *
- @example
-* // Example 1:
-* const metaContent1 = getMetadata('description');
-* console.log(metaContent1);
-* // Output: 'This is a sample description.'
-*
-* // Example 2:
-* const metaContent2 = getMetadata('og:title', anotherDocument);
-* console.log(metaContent2);
-* // Output: 'Open Graph Title'
-*/
-export function getMetadata(name: string, doc: Document = document): string {
-  const attr: 'property' | 'name' = name && name.includes(':') ? 'property' : 'name';
+ * @example
+ * // Example 1:
+ * // Assuming <meta name="description" content="This is a sample description."> exists in the document head.
+ * const metaContent = getMetadata('description');
+ * console.log(metaContent);
+ * // Output: 'This is a sample description.'
+ *
+ * @example
+ * // Example 2:
+ * // Assuming <meta property="og:title" content="Open Graph Title"> exists in the document head.
+ * const metaContent = getMetadata('og:title', document);
+ * console.log(metaContent);
+ * // Output: 'Open Graph Title'
+ */
+export function getMetadata(value: string, doc: Document = document): string {
+  const attr: 'property' | 'name' = value && value.includes(':') ? 'property' : 'name';
   const metaTags: HTMLMetaElement[] =
-    ([...doc.head.querySelectorAll(`meta[${attr}="${name}"]`)] as HTMLMetaElement[]) || [];
+    ([...doc.head.querySelectorAll(`meta[${attr}="${value}"]`)] as HTMLMetaElement[]) || [];
   const meta: string = metaTags.map((metaElement: HTMLMetaElement) => metaElement.content).join(', ');
   return meta.length ? meta : '';
 }
