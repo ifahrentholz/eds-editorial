651dbae16eff56eb02260e631f551c3f51bcce69
christian.schmidt <christian.schmidt@diva-e.com>
2024-02-27 17:45:33 +0100

    Add dynamic types for icons

---
+++ generateIconTypes.js
@@ -0,0 +1,22 @@
+const { execSync } = require('child_process');
+const fs = require('fs');
+const path = require('path');
+
+try {
+  const iconDir = './public/icons';
+  const typeFilePath = './src/icons.types.ts';
+
+  const iconFiles = fs.readdirSync(iconDir);
+  const icons = iconFiles.map((file) => path.parse(file).name.replace(/[^a-zA-Z0-9-]/g, ''));
+
+  const typeDefinition = `export type Icons = \n'${icons.join("'  | \n'")}';\n`;
+
+  fs.writeFileSync(typeFilePath, typeDefinition);
+
+  const prettierConfigPath = './.prettierrc';
+  execSync(`npx prettier --write ${typeFilePath} --config ${prettierConfigPath}`);
+
+  console.log('Icon type definitions generated and formatted successfully.');
+} catch (error) {
+  console.error('Error generating and formatting icon type definitions:', error);
+}
--- src/components/icon/icon.template.ts
+++ src/components/icon/icon.template.ts
@@ -1,5 +1,6 @@
 import { html, nothing } from 'lit';
+import { Icons } from '../../icons.types.ts';
 
-export const renderIcon = (icon: string, cssClasses?: string) => {
+export const renderIcon = (icon: Icons, cssClasses?: string) => {
   return html`<icon-component class="icon ${cssClasses ?? nothing}" name="${icon}"></icon-component>`;
 };
