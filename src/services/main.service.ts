24b0fabf8ab4ae28195436d11325047136cd924e
Christian Schmidt <151521815+LoomingEcho@users.noreply.github.com>
2024-02-27 13:13:10 +0100

    22-fix-loading-blocks-error (#45)
    * fix #22 Wrap a try catch around the import to catch the import error so the rest of the imports can still be done and the projekt does not break on one missing file.
    
    * Removing test block for on build site test
    
    * Continuous Integration Build Project
    
    * Review changes: Simplified status logic for loading blocks
    
    * Continuous Integration Build Project
    
    * Continuous Integration Build Project
    
    ---------
    
    Co-authored-by: christian.schmidt <christian.schmidt@diva-e.com>
    Co-authored-by: eds-editorial Continuous Integration <eds-editorial-ci@diva-e.com>

--- src/services/main.service.ts
+++ src/services/main.service.ts
@@ -12,6 +12,13 @@ interface LcpCandidate extends HTMLElement {
   complete: boolean;
 }
 
+class Status {
+  static unloaded = 'unloaded';
+  static loading = 'loading';
+  static loaded = 'loaded';
+  static error = 'error';
+}
+
 export class MainService {
   private lcpBlocks = ['banner'];
 
@@ -128,18 +135,23 @@ export class MainService {
   }
 
   private async loadBlockModules(block: BlockMapping) {
-    const status = block.element.dataset.blockStatus;
+    const status = block.element.dataset.blockStatus ?? Status.unloaded;
 
-    if (status !== 'loading' && status !== 'loaded') {
-      block.element.dataset.blockStatus = 'loading';
+    if (status === Status.unloaded) {
+      block.element.dataset.blockStatus = Status.loading;
 
-      const blockModule = await import(`${window.hlx.codeBasePath}/dist/${block.name}/${block.name}.js`);
+      try {
+        const blockModule = await import(`${window.hlx.codeBasePath}/dist/${block.name}/${block.name}.js`);
 
-      if (blockModule.default) {
-        await blockModule.default(block.element);
+        if (blockModule.default) {
+          await blockModule.default(block.element);
+        }
+
+        block.element.dataset.blockStatus = Status.loaded;
+      } catch (error) {
+        block.element.dataset.blockStatus = Status.error;
+        console.error('An error occurred during module import:', error);
       }
-
-      block.element.dataset.blockStatus = 'loaded';
     }
   }
 
