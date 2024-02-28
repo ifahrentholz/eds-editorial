f25be82c16bca68efbaa9370f174edf012252dc9
christian.schmidt <christian.schmidt@diva-e.com>
2024-02-28 15:50:13 +0100

    Rebuild

--- types/config.d.ts
+++ types/config.d.ts
@@ -1,8 +1,7 @@
-interface Config {
+export interface Config {
     mainTsPath: string;
     mainScssPath: string;
     fontsScssPath?: string;
     lazyStylesScssPath?: string;
 }
 export declare const config: Config;
-export {};
