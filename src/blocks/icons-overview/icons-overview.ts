9b61219a2ac138fb94c5c058791d264c635aea00
christian.schmidt <christian.schmidt@diva-e.com>
2024-02-28 12:36:33 +0100

    Refactoring icons-overview.ts

--- src/blocks/icons-overview/icons-overview.ts
+++ src/blocks/icons-overview/icons-overview.ts
@@ -3,31 +3,30 @@ import { renderIcon } from '../../components/icon/icon.template.ts';
 import { createToast } from '../../components/toast/toast.template.ts';
 import { IconName } from '../../icons.types.ts';
 
-const renderIconLabel = (icon: string) => {
-  return html`<span class="icon-label">${icon}</span>`;
-};
+const renderIconLabel = (icon: string) => html`<span class="icon-label">${icon}</span>`;
 
 const renderIconContainer = (icon: IconName, message: string, duration: number) => {
   return html`
-    <button class="icon-container" @click="${() => copyNameToClipboard(icon, message, duration)}">
+    <button type="button" class="icon-container" @click="${() => copyNameToClipboard(icon, message, duration)}">
       ${renderIcon(icon)} ${renderIconLabel(icon)}
     </button>
   `;
 };
 
-const showToast = (message: string, duration: number) => {
-  render(createToast(message, duration), document.body);
-};
+const showToast = (message: string, duration: number) => render(createToast(message, duration), document.body);
+
 const copyNameToClipboard = async (name: string, message: string, duration: number) => {
   await navigator.clipboard.writeText(name);
   showToast(message, duration);
 };
 
-const template = (iconNames: IconName[], message: string, duration: number) => {
-  return html` <div class="icons-overview">
-    ${iconNames.map((icon: IconName) => renderIconContainer(icon, message, duration))}
-  </div>`;
+const fetchIconNames = async (): Promise<IconName[]> => {
+  const iconOverview = import.meta.glob('/public/icons/*.svg');
+  return Object.keys(iconOverview).map((iconPath) => iconPath.replace(/^.*\/(.*?)\.svg$/, '$1')) as IconName[];
 };
+
+const template = (iconNames: IconName[], message: string, duration: number) =>
+  iconNames.map((icon: IconName) => renderIconContainer(icon, message, duration));
 
 const getMessage = (block: HTMLElement) => {
   const text = block.children[0].children[0];
@@ -41,15 +40,15 @@ const getDuration = (block: HTMLElement) => {
   return Number(duration.innerHTML);
 };
 
-export default function decorate(block: HTMLElement) {
-  // Vite runs on build time and finds all svg files in icons directory
-  // Since import.meta.glob only accepts literals ICON_PATH cant be used
-  const iconOverview = import.meta.glob('/public/icons/*.svg');
-  const iconNames = Object.keys(iconOverview).map((iconPath) => iconPath.replace(/^.*\/(.*?)\.svg$/, '$1'));
+const renderIcons = async (block: HTMLElement) => {
+  const iconNames = await fetchIconNames();
   const message = getMessage(block);
   const duration = getDuration(block);
 
   block.innerHTML = '';
+  render(template(iconNames, message, duration), block);
+};
 
-  render(template(iconNames as IconName[], message, duration), block);
+export default async function decorate(block: HTMLElement) {
+  await renderIcons(block);
 }
