833dbae66717ce29b817c68e244f5669b48d8faf
eds-editorial Continuous Integration <eds-editorial-ci@diva-e.com>
2024-02-28 14:17:01 +0000

    Continuous Integration Build Project

--- types/components/toast/toast.template.d.ts
+++ types/components/toast/toast.template.d.ts
@@ -1,3 +1,2 @@
-import { Toast } from './toast.ts';
 export declare const renderToast: (message: string, duration: number, cssClasses?: string) => import("lit-html").TemplateResult<1>;
-export declare const createToast: (message: string, duration: number, cssClasses?: string) => Toast;
+export declare const createToast: (message: string, duration: number, cssClasses?: string) => Promise<import("./toast.ts").Toast>;
