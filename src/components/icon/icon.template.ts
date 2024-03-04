71f7db75fee461c438af8ec14618d5d7e7d15fa9
samuelecalamita <samuelecalamita@gmail.com>
2024-03-04 14:48:18 +0100

    adjust icon overview styles

--- src/components/icon/icon.template.ts
+++ src/components/icon/icon.template.ts
@@ -1,6 +1,9 @@
-import { html, nothing } from 'lit';
+import { html } from 'lit';
 import { IconName } from '../../icons.types.ts';
 
 export const renderIcon = (icon: IconName, cssClasses?: string) => {
-  return html`<icon-component class="icon ${cssClasses ?? nothing}" name="${icon}"></icon-component>`;
+  return html`<icon-component
+    class="icon-component${cssClasses ? ` ${cssClasses}` : ''}"
+    name="${icon}"
+  ></icon-component>`;
 };
