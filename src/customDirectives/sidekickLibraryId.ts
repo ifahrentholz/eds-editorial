12ed17af2c2f77e61e4b2d63c280382a7eeadf01
Elanor Krause <elanor.krause@diva-e.com>
2024-04-15 15:24:23 +0200

    added jsdoc to sidekick lib id directive

--- src/customDirectives/sidekickLibraryId.ts
+++ src/customDirectives/sidekickLibraryId.ts
@@ -3,15 +3,30 @@ import { AttributePart, nothing } from 'lit';
 import { SidekickElement } from '../sidekickHelpers/extractSidekickLibraryId';
 import { isSidekickLibraryActive } from '../sidekickHelpers/isSidekickLibraryActive';
 
+/**
+ * Represents a directive for managing a sidekick library id.
+ * @extends Directive
+ */
 class SidekickLibraryId extends Directive {
   private part?: AttributePart;
 
+  /**
+   * Update method called when the directive is updated.
+   * @param {AttributePart} part - The attribute part to be updated.
+   * @param {unknown[]} props - Array of properties passed to the directive.
+   * @returns {unknown} - Returns the result of the render function.
+   */
   update(part: AttributePart, props: unknown[]) {
     this.part = part;
     const SidekickElement = props[0] as SidekickElement;
     return this.render(SidekickElement);
   }
 
+  /**
+   * Render method for managing Sidekick Library attributes.
+   * @param {SidekickElement} sidekickElement - Information about the Sidekick Library.
+   * @returns {unknown} - Returns the result of the render operation.
+   */
   render(sidekickElement: SidekickElement) {
     if (isSidekickLibraryActive() === false) return nothing;
 
--- types/src/customDirectives/sidekickLibraryId.d.ts
+++ types/src/customDirectives/sidekickLibraryId.d.ts
@@ -3,7 +3,18 @@ import { AttributePart } from 'lit';
 import { SidekickElement } from '../sidekickHelpers/extractSidekickLibraryId';
 declare class SidekickLibraryId extends Directive {
     private part?;
+    /**
+     * Update method called when the directive is updated.
+     * @param {AttributePart} part - The attribute part to be updated.
+     * @param {unknown[]} props - Array of properties passed to the directive.
+     * @returns {unknown} - Returns the result of the render function.
+     */
     update(part: AttributePart, props: unknown[]): symbol;
+    /**
+     * Render method for managing Sidekick Library attributes.
+     * @param {SidekickElement} sidekickElement - Information about the Sidekick Library.
+     * @returns {unknown} - Returns the result of the render operation.
+     */
     render(sidekickElement: SidekickElement): symbol;
 }
 export declare const getSidekickLibraryId: (sidekickElement: SidekickElement) => import("lit-html/directive").DirectiveResult<typeof SidekickLibraryId>;
