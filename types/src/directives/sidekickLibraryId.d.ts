b36a1baea8d8521e17c41d99f8e653dae3b916a9
eds-editorial Continuous Integration <eds-editorial-ci@diva-e.com>
2024-04-15 13:29:39 +0000

    Continuous Integration Build Project

--- types/src/directives/sidekickLibraryId.d.ts
+++ types/src/directives/sidekickLibraryId.d.ts
@@ -1,9 +1,24 @@
 import { Directive } from 'lit/directive.js';
 import { AttributePart } from 'lit';
 import { SidekickElement } from 'Helpers/sidekick/extractSidekickLibraryId';
+/**
+ * Represents a directive for managing a sidekick library id.
+ * @extends Directive
+ */
 declare class SidekickLibraryId extends Directive {
     private part?;
+    /**
+     * Update method called when the directive is updated.
+     * @param {AttributePart} part - The attribute part to be updated.
+     * @param {unknown[]} props - Array of properties passed to the directive.
+     * @returns {unknown} - Returns the result of the render function.
+     */
     update(part: AttributePart, props: unknown[]): symbol;
+    /**
+     * Render method for managing Sidekick Library attributes.
+     * @param {SidekickElement} sidekickElement - Information about the Sidekick Library.
+     * @returns {unknown} - Returns the result of the render operation.
+     */
     render(sidekickElement: SidekickElement): symbol;
 }
 export declare const getSidekickLibraryId: (sidekickElement: SidekickElement) => import("lit-html/directive").DirectiveResult<typeof SidekickLibraryId>;
