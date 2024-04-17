8bb7634887ef18ff366ab95c9e4f10d41f1fe322
eds-editorial Continuous Integration <eds-editorial-ci@diva-e.com>
2024-04-17 09:34:10 +0000

    Continuous Integration Build Project

---
+++ dist/__chunks__/hamburger.CMM0Z5X3.js
@@ -0,0 +1,2 @@
+const e="data:image/svg+xml,%3csvg%20xmlns='http://www.w3.org/2000/svg'%20fill='none'%20viewBox='0%200%2024%2024'%20stroke-width='1.5'%20stroke='currentColor'%3e%3cpath%20stroke-linecap='round'%20stroke-linejoin='round'%20d='M3.75%206.75h16.5M3.75%2012h16.5m-16.5%205.25h16.5'%20/%3e%3c/svg%3e";export{e as default};
+//# sourceMappingURL=hamburger.CMM0Z5X3.js.map
---
+++ dist/__chunks__/isSidekickLibraryActive.U-fJCMMj.js
@@ -0,0 +1,2 @@
+const r=()=>{const i=document.querySelector("#main");return i?window.location.href==="about:srcdoc"&&i.classList.contains("sidekick-library"):!1};export{r as i};
+//# sourceMappingURL=isSidekickLibraryActive.U-fJCMMj.js.map
