37ee43de2744fe744058db0d7b0635a8b78453df
izabela.horodenska <izabla.horodenska@diva-e.com>
2024-04-18 10:01:53 +0200

    navigation menu linking to sections

--- styleguide/js/script.js
+++ styleguide/js/script.js
@@ -1,4 +1,17 @@
 document.addEventListener('DOMContentLoaded', function () {
+  const navLinks = document.querySelectorAll('.ls-sidebar-element');
+
+  navLinks.forEach(function (link) {
+    link.addEventListener('click', function (event) {
+      const targetId = link.getAttribute('href').substring(1);
+      const targetElement = document.getElementById(targetId);
+
+      if (targetElement) {
+        event.preventDefault();
+        targetElement.scrollIntoView({ behavior: 'smooth' });
+      }
+    });
+  });
   const xhr = new XMLHttpRequest();
   console.log(xhr);
   xhr.onreadystatechange = function () {
