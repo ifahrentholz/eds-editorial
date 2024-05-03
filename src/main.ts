5c8e46c8cc435f77b7d683e6274f5f8d4770df3b
Ingo Fahrentholz <ingof.work@gmail.com>
2024-05-03 10:30:24 +0200

    fix init loading and wait for document (main) before fire HLX script

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,9 @@ import './components/sidebar/sidebar.init.ts';
 import './components/innerContainer/innerContainer.init.ts';
 import './components/header.ts';
 import './components/icon/icon.ts';
-import HLX from './app/index.ts';
+import { initHLXApp } from './app/init.ts';
 
-HLX.addBeforeEagerTask(() => {
-  const main = document.getElementsByTagName('main')[0];
-  main.setAttribute('id', 'main');
-  return Promise.resolve();
-});
-
-HLX.init();
+initHLXApp();
 
 declare global {
   interface Window {
