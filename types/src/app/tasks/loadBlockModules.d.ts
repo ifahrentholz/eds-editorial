e054a45dd36908e4f3bb75ccbcccd30a4d0a83ee
christian.schmidt <christian.schmidt@diva-e.com>
2024-05-07 14:36:20 +0200

    Rebuild

--- types/src/app/tasks/loadBlockModules.d.ts
+++ types/src/app/tasks/loadBlockModules.d.ts
@@ -1,2 +1,7 @@
 import { BlockMapping } from '../app.types';
+/**
+ * Load the es module for the block. The module should be named as the block name.
+ * @param block - The block to load the module for.
+ * @returns Promise<void>
+ */
 export declare function loadBlockModules(block: BlockMapping): Promise<void>;
