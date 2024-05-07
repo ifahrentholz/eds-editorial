04b1b27632ccc5a1eaa06f316563f2805f921d06
christian.schmidt <christian.schmidt@diva-e.com>
2024-05-07 13:20:21 +0200

    Refactor loadCss * Remove redundant async since a new Promise gets returned * link.onload calls resolve without passing event * Removed unused resolve true value

--- src/app/tasks/loadCSS.ts
+++ src/app/tasks/loadCSS.ts
@@ -5,7 +5,7 @@ import { getUrlForEndpoint } from '../utils/getUrlForEndpoint';
  * @param endpoint - The endpoint of the CSS file.
  * @returns Promise<boolean>
  */
-export async function loadCSS(endpoint: string) {
+export function loadCSS(endpoint: string): Promise<void> {
   return new Promise((resolve, reject) => {
     const { href } = getUrlForEndpoint(endpoint);
 
@@ -13,11 +13,11 @@ export async function loadCSS(endpoint: string) {
       const link = document.createElement('link');
       link.rel = 'stylesheet';
       link.href = href;
-      link.onload = resolve;
+      link.onload = () => resolve();
       link.onerror = reject;
       document.head.append(link);
     } else {
-      resolve(true);
+      resolve();
     }
   });
 }
